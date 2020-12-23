e0ec2d1d843ca7455a5c82999096221af56bbb18
Ludovic Gindre <ludovic@gind.re>
2020-12-23 12:01:03 +0100

    Mocked quote unmarshaling

--- pkg/tpm/tests/mocks/Quote.go
+++ pkg/tpm/tests/mocks/Quote.go
@@ -7,6 +7,7 @@ import (
 type MockQuote struct {
 	CatchVerify     func(ak tpm.AttestationKey, nonce []byte) error
 	CatchVerifyPCRs func(pcrs []tpm.PCR) error
+	CatchUnmarshal  func(data []byte) error
 }
 
 var _ tpm.Quote = (*MockQuote)(nil) // Verify that *MockQuote implements Quote.
@@ -17,4 +18,8 @@ func (q *MockQuote) Verify(ak tpm.AttestationKey, nonce []byte) error {
 
 func (q *MockQuote) VerifyPCRs(pcrs []tpm.PCR) error {
 	return q.CatchVerifyPCRs(pcrs)
+}
+
+func (q *MockQuote) UnmarshalJSON(data []byte) error {
+	return q.CatchUnmarshal(data)
 }
