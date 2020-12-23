1bfd2b24f46874cdb468c09455b6aeb7b4f5b2c4
Ludovic Gindre <ludovic@gind.re>
2020-12-23 12:02:46 +0100

    Improved prover RestServer tests

--- internal/prover/RestServer/RestServer_test.go
+++ internal/prover/RestServer/RestServer_test.go
@@ -2,40 +2,37 @@ package RestServer
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/google/go-cmp/cmp"
 	"github.com/gorilla/mux"
 	log "github.com/sirupsen/logrus"
 	"github.com/xcaliburne/RemoteAttestations/internal/prover"
+	"github.com/xcaliburne/RemoteAttestations/internal/prover/tests/mocks"
 	"github.com/xcaliburne/RemoteAttestations/internal/verifier/tests/fakes"
 	"github.com/xcaliburne/RemoteAttestations/pkg/httpClient"
 	"github.com/xcaliburne/RemoteAttestations/pkg/tests"
 	"github.com/xcaliburne/RemoteAttestations/pkg/tpm"
-	"github.com/xcaliburne/RemoteAttestations/pkg/tpm/tests/mocks"
+	tpmFakes "github.com/xcaliburne/RemoteAttestations/pkg/tpm/tests/fakes"
+	mocksTPM "github.com/xcaliburne/RemoteAttestations/pkg/tpm/tests/mocks"
+	"io/ioutil"
 	"net"
 	"net/http"
+	"net/http/httptest"
 	"net/url"
 	"testing"
 	"time"
 )
 
 var r *RestServer
-var p = &prover.Prover{
+var p = &prover.DataProver{
 	Config: &prover.Config{
 		Name:          "test",
 		AKFile:        "ak.json",
 		OwnerPassword: "tpmOwnerPassword",
 		UserPassword:  "tpmUserPassword",
 		VerifierAddress: &url.URL{
-			Scheme:      "http",
-			Opaque:      "",
-			User:        nil,
-			Host:        "127.0.0.1",
-			Path:        "",
-			RawPath:     "",
-			ForceQuery:  false,
-			RawQuery:    "",
-			Fragment:    "",
-			RawFragment: "",
+			Scheme: "http",
+			Host:   "127.0.0.1",
 		},
 	},
 	TPM: nil,
@@ -48,6 +45,7 @@ var config = &Config{
 }
 
 func init() {
+	log.SetOutput(ioutil.Discard)
 	var err error
 	r, err = NewServer(config, p)
 	if err != nil {
@@ -82,59 +80,87 @@ func TestNewServer(t *testing.T) {
 }
 
 func TestRestServer_attest(t *testing.T) {
-	var want tpm.Quote
-	var wantStatus, gotStatus int
-	var got *tpm.QuoteData
-	var wantErr, gotErr error
-
-	mockTPM := mocks.MockTPM{}
-	p.TPM = &mockTPM
-
-	r.Run()
-	defer func() {
-		if err := r.Stop(); err != nil {
-			log.Errorf("error closing server: %v", err)
-		}
-	}()
-	time.Sleep(1 * time.Second) //wait for server to be ready
-
-	want, wantErr = mocks.GetFakeQuote(), nil
-	mockTPM.CatchQuote = func(ak tpm.AttestationKey, nonce []byte, pcrIds []int) (tpm.Quote, error) {
-		return want, nil
-	}
-	nonce := fakes.GetFakeNonce()
-	jsonBody, err := json.Marshal(
-		struct {
-			Nonce []byte
-		}{Nonce: nonce},
-	)
+	jsonQuote, err := json.Marshal(tpmFakes.GetFakeQuote())
 	if err != nil {
-		t.Error(err)
+		t.Fatalf("Unable to marshal quote: %v", err)
 	}
-	//Test valid query
-	req, gotErr := httpClient.Client.Post("http://"+r.server.Addr+"/attest", "application/json", jsonBody)
-	if gotErr != wantErr {
-		t.Error(tests.Failure(t, gotErr, wantErr, ""))
-		t.Skip()
-	}
-	decoder := json.NewDecoder(req.Body)
-	err = decoder.Decode(&got)
-	if err != nil {
-		t.Error(err)
-	}
-	if !cmp.Equal(got, want) {
-		t.Error(tests.Failure(t, got, want, ""))
+	var testSuite = []struct {
+		name       string
+		input      string
+		mock       mocks.MockProver
+		want       []byte
+		wantErr    error
+		wantStatus int
+	}{
+		{
+			name:       "valid query",
+			input:      fmt.Sprintf("{\"Nonce\": \"%v\"}", string(fakes.GetFakeNonce())),
+			mock:       mocks.MockProver{CatchAttest: func(nonce []byte) (tpm.Quote, error) { return tpmFakes.GetFakeQuote(), nil }},
+			want:       jsonQuote,
+			wantErr:    nil,
+			wantStatus: http.StatusOK,
+		},
+		{
+			name:       "query without nonce",
+			input:      fmt.Sprintf("{\"Nonce\": \"%v\"}", ""),
+			mock:       mocks.MockProver{CatchAttest: func(nonce []byte) (tpm.Quote, error) { return tpmFakes.GetFakeQuote(), nil }},
+			want:       []byte{},
+			wantErr:    nil,
+			wantStatus: http.StatusBadRequest,
+		},
+		{
+			name:       "query with invalid json",
+			input:      fmt.Sprintf("{\"Nonce\": \"%v\"}", "{{"),
+			mock:       mocks.MockProver{CatchAttest: func(nonce []byte) (tpm.Quote, error) { return tpmFakes.GetFakeQuote(), nil }},
+			want:       []byte{},
+			wantErr:    nil,
+			wantStatus: http.StatusBadRequest,
+		},
+		{
+			name:       "query with internal error",
+			input:      fmt.Sprintf("{\"Nonce\": \"%v\"}", string(fakes.GetFakeNonce())),
+			mock:       mocks.MockProver{CatchAttest: func(nonce []byte) (tpm.Quote, error) { return nil, fmt.Errorf("some error") }},
+			want:       []byte{},
+			wantErr:    nil,
+			wantStatus: http.StatusInternalServerError,
+		},
+		{
+			name:  "query with marshaling error",
+			input: fmt.Sprintf("{\"Nonce\": \"%v\"}", string(fakes.GetFakeNonce())),
+			mock: mocks.MockProver{CatchAttest: func(nonce []byte) (tpm.Quote, error) {
+				return &mocksTPM.MockQuote{
+					CatchUnmarshal: func(data []byte) error {
+						return fmt.Errorf("some error")
+					},
+				}, nil
+			}},
+			want:       []byte{},
+			wantErr:    nil,
+			wantStatus: http.StatusInternalServerError,
+		},
 	}
 
-	//Test query without nonce
-	want, wantErr, wantStatus = nil, nil, http.StatusInternalServerError
-	req, gotErr = httpClient.Client.Post("http://"+r.server.Addr+"/attest", "application/json", []byte("{}"))
-	if gotErr != wantErr {
-		t.Error(tests.Failure(t, gotErr, wantErr, ""))
-		t.Skip()
-	}
-	gotStatus = req.StatusCode
-	if gotStatus != wantStatus {
-		t.Error(tests.Failure(t, gotStatus, wantStatus, ""))
+	var gotStatus int
+	var got []byte
+
+	testServer := httptest.NewServer(http.HandlerFunc(r.attest))
+	defer testServer.Close()
+
+	for _, test := range testSuite {
+		t.Run(test.name, func(t *testing.T) {
+			r.p = &test.mock
+			req, gotErr := httpClient.Client.Post(testServer.URL, "application/json", []byte(test.input))
+			if gotErr != test.wantErr {
+				t.Error(tests.Failure(t, gotErr, test.wantErr, ""))
+			}
+			got, _ = ioutil.ReadAll(req.Body)
+			gotStatus = req.StatusCode
+			if gotStatus != test.wantStatus {
+				t.Error(tests.Failure(t, gotStatus, test.wantStatus, ""))
+			}
+			if len(test.want) != 0 && !cmp.Equal(got, test.want) {
+				t.Error(tests.Failure(t, got, test.want, ""))
+			}
+		})
 	}
 }
